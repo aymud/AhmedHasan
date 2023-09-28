b88dea73ba892056e0bf56f54a10b24b02d5e99c
Ahmed <aaa26770123@gmail.com>
2023-09-28 11:31:51 -0600

    Add skills section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let typed = new Typed(".typing-text", {
-  strings: ["programmer.", "developer."],
+  strings: ["a programmer.", "a developer.", "also amazing!"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 100,
